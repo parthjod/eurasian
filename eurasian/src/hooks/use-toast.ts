350f382b893ca47a89114d801155b63133e068fd
parthjod <hlomello765@gmail.com>
2025-06-03 15:36:26 +0530

    features implementing ohne style

--- eurasian/src/hooks/use-toast.ts
+++ eurasian/src/hooks/use-toast.ts
@@ -15,13 +15,6 @@ export interface ToasterToast extends ToastProps {
 const TOAST_LIMIT = 1;
 const TOAST_REMOVE_DELAY = 1000000;
 
-const ACTION_TYPES = {
-  ADD_TOAST: "ADD_TOAST",
-  UPDATE_TOAST: "UPDATE_TOAST",
-  DISMISS_TOAST: "DISMISS_TOAST",
-  REMOVE_TOAST: "REMOVE_TOAST",
-} as const;
-
 let count = 0;
 
 function genId(): string {
@@ -29,23 +22,21 @@ function genId(): string {
   return count.toString();
 }
 
-type ActionType = typeof ACTION_TYPES;
-
 type Action =
   | {
-      type: ActionType["ADD_TOAST"];
+      type: "ADD_TOAST";
       toast: ToasterToast;
     }
   | {
-      type: ActionType["UPDATE_TOAST"];
+      type: "UPDATE_TOAST";
       toast: Partial<ToasterToast> & { id: string };
     }
   | {
-      type: ActionType["DISMISS_TOAST"];
+      type: "DISMISS_TOAST";
       toastId?: string;
     }
   | {
-      type: ActionType["REMOVE_TOAST"];
+      type: "REMOVE_TOAST";
       toastId?: string;
     };
 
